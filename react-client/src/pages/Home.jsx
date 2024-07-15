c0f2c832dabd53d40ddcfc8abe1557a7e9a84aff
lambda-mena <juanmena1605@gmail.com>
2024-07-15 11:45:43 -0500

    style: add inter font & improve headings on home

--- react-client/src/pages/Home.jsx
+++ react-client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function HomePage() {
   return (
     <>
-      <h1 className='block text-5xl font-bold text-center mb-9'>WheelZ Admin</h1>
+      <h1 className='block text-5xl font-bold text-center mb-9 text-cyan-700'>WheelZ Admin</h1>
       <div className='flex flex-col gap-y-2 w-1/2 mx-auto'>
         <Button as={Link} to='/vehiculos'>Gestionar Vehiculos</Button>
         <Button as={Link} to='/seguros'>Gestionar Seguros</Button>
