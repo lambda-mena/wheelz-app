52aec19ced046601923d9b7f7c41330ddcd8a190
lambda-mena <juanmena1605@gmail.com>
2024-07-16 11:39:44 -0500

    refactor: improve header component & update favicon

--- react-client/src/components/Header/Header.jsx
+++ react-client/src/components/Header/Header.jsx
@@ -1,20 +1,19 @@
-import { Avatar, Navbar } from 'flowbite-react';
-import defaultAvatar from '../../assets/img/default-avatar.png';
+import { Avatar, Navbar } from 'flowbite-react'
+import defaultAvatar from '../../assets/img/default-avatar.png'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <Navbar fluid rounded>
       <Navbar.Brand as={Link} to='/'>
-        <img src='./favicon.ico' alt='wheelz-icon' />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">WheelZ</span>
+        <span className="self-center whitespace-nowrap text-xl font-bold text-cyan-700">WheelZ</span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Avatar img={defaultAvatar} alt='default-avatar' />
+        <Avatar className='me-1' img={defaultAvatar} alt='default-avatar' />
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link as={Link} to='/' active>Panel de Administración</Navbar.Link>
+        <Navbar.Link as={Link} to='/' active>Panel Administrativo</Navbar.Link>
         <Navbar.Link as={Link} to='/vehiculos'>Vehiculos</Navbar.Link>
         <Navbar.Link as={Link} to='/seguros'>Seguros</Navbar.Link>
         <Navbar.Link as={Link} to='/usuarios'>Usuarios</Navbar.Link>
